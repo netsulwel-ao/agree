eb699ef20d53b3b10f1f6c291a1a277f7b3e5050
kiatde <kitade.org@gmail.com>
2026-06-06 17:40:34 +0100

    main

---
+++ supabase/migrations/20260604000007_disable_rls_user_signatures.sql
@@ -0,0 +1,5 @@
+-- ============================================
+-- Disable RLS on user_signatures table
+-- ============================================
+
+ALTER TABLE user_signatures DISABLE ROW LEVEL SECURITY;
